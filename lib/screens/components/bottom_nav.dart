b96d5930cd816dd43a7cabbbbb03c0fbec835833
RJ2607 <rishijain2607@gmail.com>
2024-02-13 22:58:31 +0530

    Add BottomNav component

---
+++ lib/screens/components/bottom_nav.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+class BottomNav extends StatefulWidget {
+  BottomNav({
+    Key? key,
+    required this.selectedIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  int selectedIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(.1),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            rippleColor: Colors.grey[300]!,
+            hoverColor: Colors.grey[100]!,
+            gap: 2.9,
+            activeColor: Colors.black,
+            iconSize: 23,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(milliseconds: 400),
+            tabBackgroundColor: Colors.grey[100]!,
+            color: Colors.white,
+            tabs: [
+              GButton(
+                icon: LineIcons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: LineIcons.heartbeat,
+                text: 'Diagnosis',
+              ),
+              GButton(
+                icon: LineIcons.running,
+                text: 'Exercise',
+              ),
+              GButton(
+                icon: LineIcons.user,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: widget.selectedIndex,
+            onTabChange: (index) {
+              widget.onTap(index);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
