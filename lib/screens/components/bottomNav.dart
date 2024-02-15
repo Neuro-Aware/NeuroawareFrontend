b9211ad4cd7f87bad4c3af04273c8e59d84435a0
RJ2607 <rishijain2607@gmail.com>
2024-02-16 01:39:20 +0530

    Refactor bottom navigation bar styling

--- lib/screens/components/bottomNav.dart
+++ lib/screens/components/bottomNav.dart
@@ -19,51 +19,53 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black,
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 20,
-            color: Colors.black.withOpacity(.1),
-          )
-        ],
-      ),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-          child: GNav(
-            rippleColor: Colors.grey[300]!,
-            hoverColor: Colors.grey[100]!,
-            gap: 2.9,
-            activeColor: Colors.black,
-            iconSize: 23,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            duration: Duration(milliseconds: 400),
-            tabBackgroundColor: Colors.grey[100]!,
-            color: Colors.white,
-            tabs: [
-              GButton(
-                icon: LineIcons.home,
-                text: 'Home',
-              ),
-              GButton(
-                icon: LineIcons.heartbeat,
-                text: 'Diagnosis',
-              ),
-              GButton(
-                icon: LineIcons.fire,
-                text: 'Analytics',
-              ),
-              GButton(
-                icon: LineIcons.user,
-                text: 'Profile',
-              ),
-            ],
-            selectedIndex: widget.selectedIndex,
-            onTabChange: (index) {
-              widget.onTap(index);
-            },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 8),
+            child: GNav(
+              gap: 2.9,
+              activeColor: Colors.black,
+              iconSize: 22,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Color.fromARGB(255, 90, 224, 95),
+              color: Colors.white,
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.heartbeat,
+                  text: 'Diagnosis',
+                ),
+                GButton(
+                  icon: LineIcons.fire,
+                  text: 'Analytics',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: widget.selectedIndex,
+              onTabChange: (index) {
+                widget.onTap(index);
+              },
+            ),
           ),
         ),
       ),
