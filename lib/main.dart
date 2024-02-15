49e2f71cb91a9c20e3ec1706b0cdc39dc814826c
RJ2607 <rishijain2607@gmail.com>
2024-02-16 01:39:11 +0530

    Update app theme and scaffold background color

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(fontFamily: 'Raleway', ),
+        theme: ThemeData(
+          fontFamily: 'Raleway',
+          scaffoldBackgroundColor: Color.fromARGB(255, 237, 237, 237),
+        ),
         debugShowCheckedModeBanner: false,
         home: login ? MainPage() : StartScreen());
   }
