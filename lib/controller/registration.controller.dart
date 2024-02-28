1fd82d5b1555938ed52466b5d47bc6752eaf231a
RJ2607 <rishijain2607@gmail.com>
2024-02-28 16:11:49 +0530

    Refactor registration controller and add error handling

--- lib/controller/registration.controller.dart
+++ lib/controller/registration.controller.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:neuroaware/utils/API_endpoints.dart';
@@ -9,32 +10,35 @@ class RegistrationController {
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
 
-  Future<Map<String, dynamic>> registerUser() async {
-    final response = await http.post(
-      Uri.parse(
-          '${ApiEndPoints.baseUrl}/${ApiEndPoints.authEndpoints.registerEmail}'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'username': usernameController.text,
-        'email': emailController.text,
-        'password': passwordController.text,
-        'confirmPassword': confirmPasswordController.text,
-      }),
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to register user');
+  void dispose() {
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+  }
+
+  Future<List> registerUser() async {
+    var url =
+        '${ApiEndPoints.baseUrl}/${ApiEndPoints.authEndpoints.registerEmail}';
+    print(url);
+    try {
+      var response = await http.post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'username': usernameController.text.trim(),
+          'email': emailController.text.trim(),
+          'password': passwordController.text.trim(),
+          'confirmPassword': confirmPasswordController.text.trim(),
+        }),
+      );
+
+      return [response.statusCode, response.body];
+    } catch (e) {
+      print(e);
+      return [500, 'Internal Server Error'];
     }
-    // return {
-    //   'message': 'User registered successfully',
-    //   'status': 'success',
-    //   'data': {
-    //     'email': email,
-    //     'password': password,
-    //   }
-    // };
   }
 }
