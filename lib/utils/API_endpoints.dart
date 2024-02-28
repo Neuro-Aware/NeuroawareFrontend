dbd29f75c65f4383105855cbcb07f893cd05dfb8
RJ2607 <rishijain2607@gmail.com>
2024-02-28 16:11:56 +0530

    Update API base URL and add a test variable

--- lib/utils/API_endpoints.dart
+++ lib/utils/API_endpoints.dart
@@ -1,9 +1,11 @@
 class ApiEndPoints {
-  static final String baseUrl = 'http://localhost:3000';
+  static final String baseUrl =
+      'https://fbea-2402-3a80-8df-c07d-b822-789-7a2d-9199.ngrok-free.app';
   static _AuthEndPoints authEndpoints = _AuthEndPoints();
 }
 
 class _AuthEndPoints {
+  final String test = 'test';
   final String registerEmail = 'auth/register';
   final String loginEmail = 'auth/login';
 }
