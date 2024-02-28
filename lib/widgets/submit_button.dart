8452997b405f5781d380f1290ef3f0bebf037e43
RJ2607 <rishijain2607@gmail.com>
2024-02-28 16:12:05 +0530

    Add loading indicator to SubmitButton

--- lib/widgets/submit_button.dart
+++ lib/widgets/submit_button.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class SubmitButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
-  const SubmitButton({Key? key, required this.onPressed, required this.title})
+  final bool loading;
+  const SubmitButton(
+      {Key? key,
+      required this.onPressed,
+      required this.title,
+      this.loading = false})
       : super(key: key);
 
   @override
@@ -12,15 +17,19 @@ class SubmitButton extends StatelessWidget {
       width: double.infinity,
       height: 45,
       child: OutlinedButton(
+          //create onPressed function to navigation to login page after successful registration
           onPressed: onPressed,
           style: OutlinedButton.styleFrom(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10)),
               backgroundColor: Color.fromRGBO(40, 65, 98, 1)),
-          child: Text(
-            title,
-            style: TextStyle(color: Colors.white, fontSize: 15),
-          )),
+          child: loading
+              ? CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white))
+              : Text(
+                  title,
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                )),
     );
   }
 }
